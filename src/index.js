266d0bdf1b1e3ea3bf259fc73559785cc3b893e9
Vermium Sifell <vermium@zyner.org>
2021-03-29 16:58:04 +0200

    feat: :rocket: add discord.js with event ready
    add npm module discord.js, remove log Hello world!, add config example, add new main code (with client ready) that includes basic discord.js bot login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const {
+  token, status, activity, prefix,
+} = require('./config/discord.json');
+
+client.on('ready', () => {
+  client.user.setStatus(status);
+  const { message, url, type } = activity;
+  client.user.setActivity(message, {
+    url,
+    type,
+  });
+  console.log('Launching on prefix', prefix);
+});
+
+client.login(token);
